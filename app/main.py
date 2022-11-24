0de8a80fee69e760536a716580d730c3e44a256d
Jinsu Ha <korean_js@naver.com>
2022-11-24 14:05:55 +0900

    feat: added router to the app

--- app/main.py
+++ app/main.py
@@ -1,60 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from typing import List
-from starlette.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from .db import session
 from .model import User, UserTable
+from .dependencies import get_query_token, get_token_header
+from .routers import users
 
-app = FastAPI()
+app = FastAPI(dependencies=[Depends(get_query_token)])
+
+
+app.include_router(users.router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8000",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-@app.get("/users")
-def get_user_list():
-    user_list = session.query(UserTable).all()
-    return {"userList" : user_list}
-
-
-@app.get("/users/{user_id}")
-def get_user(user_id: int):
-    user = session.query(UserTable).filter(UserTable.id == user_id).first()
-    return user
-
-
-@app.post("/users")
-def create_user(name: str, age: int):
-
-    user = UserTable()
-    user.name = name
-    user.age = age
-
-    session.add(user)
-    session.commit()
-
-    return f"{name} created"
-
-
-@app.put("/users/")
-def update_user(users: List[User]):
-
-    for i in users:
-        user = session.query(UserTable).filter(UserTable.id == i.id).first()
-        user.name = i.name
-        user.age = i.age
-        session.commit()
-        
-    return user
-
-
-@app.delete("/users/{user_id}")
-def delete_user(user_id: int):
-    user = session.query(UserTable).filter(UserTable.id == user_id).delete()
-    session.commit()
-    return "User is deleted"
+@app.get("/")
+async def index():
+    return {"message": "Ok"}
---
+++ app/routers/users.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter, Depends, HTTPException
+from typing import List
+
+from ..dependencies import get_token_header
+from ..model import User, UserTable
+from ..db import session
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"],
+    dependencies=[Depends(get_token_header)],
+    responses={404: {"description": "Not found"}}
+)
+
+
+@router.get("")
+def get_user_list():
+    user_list = session.query(UserTable).all()
+    return {"userList" : user_list}
+
+
+@router.get("/{user_id}")
+def get_user(user_id: int):
+    user = session.query(UserTable).filter(UserTable.id == user_id).first()
+    return user
+
+
+@router.post("/")
+def create_user(name: str, age: int):
+
+    user = UserTable()
+    user.name = name
+    user.age = age
+
+    session.add(user)
+    session.commit()
+
+    return f"{name} created"
+
+
+@router.put("/")
+def update_user(users: List[User]):
+
+    for i in users:
+        user = session.query(UserTable).filter(UserTable.id == i.id).first()
+        user.name = i.name
+        user.age = i.age
+        session.commit()
+        
+    return user
+
+
+@router.delete("/{user_id}")
+def delete_user(user_id: int):
+    user = session.query(UserTable).filter(UserTable.id == user_id).delete()
+    session.commit()
+    return "User is deleted"
